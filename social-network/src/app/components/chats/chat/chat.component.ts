3bf720d4eb7c19f4716821262a28732a63cdd439
kivlaros <kivlaros@gmail.com>
2023-02-08 17:18:43 +0300

    fix: delete some comments

--- social-network/src/app/components/chats/chat/chat.component.ts
+++ social-network/src/app/components/chats/chat/chat.component.ts
@@ -1,8 +1,7 @@
 import { FormGroup, FormControl } from '@angular/forms';
 import { IChat } from './../../../models/types';
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { EventSourcePolyfill } from 'ng-event-source';
 import { SseService } from 'src/app/services/sse.service';
 import { ChatsService } from 'src/app/services/chats.service';
 
@@ -14,10 +13,6 @@ import { ChatsService } from 'src/app/services/chats.service';
 export class ChatComponent implements OnInit, OnDestroy {
   chat!: IChat;
   form!: FormGroup;
-  //eventSource = new EventSourcePolyfill(`http://localhost:5000/chats/sse/${this.chat._id}`, {headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiX2lkIjoiNjNlMjEzNmFkY2RmNmRhODYwNzQyOTI5IiwiaWF0IjoxNjc1NzY2ODE5LCJleHAiOjE2NzU4NTMyMTl9.n1oBRJUv9nsL9x7tplWVWit1zDjxuNdG0xxofZrTrGM' }});
-  // eventSource = new EventSource(
-  //   'http://localhost:5000/chats/sse/63e2316edcdf6da86074296d'
-  // );
   constructor(
     private route: ActivatedRoute,
     private sseService: SseService,
