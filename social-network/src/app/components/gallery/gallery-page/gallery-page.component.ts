b6d64a4349d651187904db2b8a9820cce00b8f05
kivlaros <kivlaros@gmail.com>
2023-02-10 17:35:37 +0300

    feat: add file upload to gallery

--- social-network/src/app/components/gallery/gallery-page/gallery-page.component.ts
+++ social-network/src/app/components/gallery/gallery-page/gallery-page.component.ts
@@ -1,7 +1,9 @@
 import { IImage } from './../../../models/types';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ChatsService } from 'src/app/services/chats.service';
+import { GalleryService } from 'src/app/services/gallery.service';
 
 @Component({
   selector: 'app-gallery-page',
@@ -10,12 +12,57 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class GalleryPageComponent implements OnInit {
   imgList: IImage[];
-  constructor(private route: ActivatedRoute) {}
+  form: FormGroup;
+  error: { statusCode: number; message: string } | null;
+  @ViewChild('file') file: ElementRef;
+  isDisabled: boolean = false;
+  constructor(
+    private route: ActivatedRoute,
+    private galleryService: GalleryService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.route.data.subscribe((data) => {
-      console.log(data);
       this.imgList = data['data'];
+      console.log(this.imgList);
     });
+    this.form = this.formBuilder.group({
+      files: [''],
+    });
+  }
+
+  onFileChange(event: any) {
+    console.log('change');
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.form.get('files')!.setValue(file);
+      console.log(this.form);
+      this.error = null;
+    }
+  }
+
+  onSubmit() {
+    this.isDisabled = true;
+    const formData = new FormData();
+    formData.append('files', this.form.get('files')!.value);
+
+    this.galleryService.upload(formData).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.imgList = data;
+        this.file.nativeElement.value = null;
+        this.isDisabled = false;
+      },
+      error: (e) => {
+        console.log(e);
+        this.error = e;
+        this.isDisabled = false;
+      },
+    });
+  }
+
+  getImageLink(image: IImage) {
+    return { link: `http://localhost:5000/${image.imgLink}` };
   }
 }
