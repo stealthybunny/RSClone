deaa8b038d5073a81d23b357810ecc00a262f8ef
Peter Zaichyk <zaichikpetr@gmail.com>
2023-02-14 18:52:21 +0300

    feat: add subscriptions section

---
+++ social-network/src/app/components/subscribtions/subscribtions.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/app/models/types';
+import { LoginServiceService } from 'src/app/services/login-service.service';
+import { pathToAPI } from 'src/app/store';
+
+@Component({
+  selector: 'app-subscribtions',
+  templateUrl: './subscribtions.component.html',
+  styleUrls: ['./subscribtions.component.scss']
+})
+export class SubscribtionsComponent implements OnInit{
+  token: string;
+  subscriptions: IUser[];
+  api: string;
+  
+
+  constructor(private loginService: LoginServiceService) {
+
+  }
+  ngOnInit(): void {
+    this.api = pathToAPI
+    const authInfo = JSON.parse(window.localStorage.getItem('RSClone-socnetwork') as string)
+    this.token = authInfo.token;
+    const token: string = JSON.parse(window.localStorage.getItem('RSClone-socnetwork') as string).token;
+    this.loginService.getUsers(token).subscribe((data) => {
+      const dataArr = data.filter(user => user._id === authInfo._id);
+
+      this.subscriptions = dataArr[0].subscriptions;
+      console.log(this.subscriptions)
+    })
+  }
+
+}
