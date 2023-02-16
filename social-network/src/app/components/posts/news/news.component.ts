41746457b5e95aca6d71756ab6fd873acc34c145
kivlaros <kivlaros@gmail.com>
2023-02-16 22:14:58 +0300

    feat: implemented the ability to put likes and write comments

--- social-network/src/app/components/posts/news/news.component.ts
+++ social-network/src/app/components/posts/news/news.component.ts
@@ -6,23 +6,22 @@ import { pathToAPI } from 'src/app/store';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+  styleUrls: ['./news.component.scss'],
 })
 export class NewsComponent implements OnInit {
   newsList: IPost[];
-  isOpen = false;
+  isOpen: boolean[];
+  isDisabled = false;
   api = pathToAPI;
-  constructor(
-    private newsServes: NewsService,
-  ) {
-
-  }
+  constructor(private newsServes: NewsService) {}
 
   ngOnInit(): void {
     this.newsServes.getAllNewsList().subscribe({
       next: (data) => {
         console.log(data);
-        this.newsList = data; 
+        this.newsList = data;
+        this.isOpen = Array.from(this.newsList, (e) => false);
+        console.log(this.isOpen);
       },
       error: (e) => {
         console.log(e);
@@ -30,9 +29,22 @@ export class NewsComponent implements OnInit {
     });
   }
 
+  sendLike(id: string, index: number) {
+    this.newsServes.postLike(id).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.newsList[index] = data;
+        this.isDisabled = false;
+      },
+      error: (e) => {
+        console.log(e);
+        this.isDisabled = false;
+      },
+    });
+  }
+
   getComments(post: IPost) {
     console.log(post);
-    console.log(document.querySelectorAll('app-comments'))
+    console.log(document.querySelectorAll('app-comments'));
   }
 }
-
